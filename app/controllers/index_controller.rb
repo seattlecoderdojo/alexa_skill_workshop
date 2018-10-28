0b160b32da88a7df52f226808124c248225f040a
cwwllac <cwwllac@amazon.com>
2018-10-28 01:42:15 -0400

    add retries to assume role

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -4,30 +4,36 @@ class IndexController < ApplicationController
     # create a signin link
     unless current_user.nil?
       require 'open-uri'
+      retries ||= 0
 
       signin_url = 'https://signin.aws.amazon.com/federation'
       role = "arn:aws:iam::051946164308:role/#{@user.role}"
       console_url = "https://console.aws.amazon.com/lambda/home?region=us-east-1#/functions/#{@user.role}"
 
-      sts = Aws::STS::Client.new
-      creds = sts.assume_role(role_arn: role,
-                            role_session_name: @user.name,
-                            external_id: @user.uid).credentials
-      session = {
-        sessionId: creds.access_key_id,
-        sessionKey: creds.secret_access_key,
-        sessionToken: creds.session_token
-      }.to_json
+      begin
+        sts = Aws::STS::Client.new
+        creds = sts.assume_role(role_arn: role,
+                                role_session_name: @user.name,
+                                external_id: @user.uid).credentials
+        session = {
+          sessionId: creds.access_key_id,
+          sessionKey: creds.secret_access_key,
+          sessionToken: creds.session_token
+        }.to_json
 
-      get_uri = "#{signin_url}?Action=getSigninToken&SessionType=json&Session=#{CGI.escape(session)}"
+        get_uri = "#{signin_url}?Action=getSigninToken&SessionType=json&Session=#{CGI.escape(session)}"
 
-      signin_token = JSON.parse(URI.parse(get_uri).read)['SigninToken']
-      signin_token_param = "&SigninToken=#{CGI.escape(signin_token)}"
+        signin_token = JSON.parse(URI.parse(get_uri).read)['SigninToken']
+        signin_token_param = "&SigninToken=#{CGI.escape(signin_token)}"
 
-      issuer_param = "&Issuer=#{CGI.escape('https://alexa.seattlecoderdojo.org')}"
-      destination_param = "&Destination=#{CGI.escape(console_url)}"
+        issuer_param = "&Issuer=#{CGI.escape('https://alexa.seattlecoderdojo.org')}"
+        destination_param = "&Destination=#{CGI.escape(console_url)}"
 
-      @login_uri = "#{signin_url}?Action=login#{signin_token_param}#{issuer_param}#{destination_param}"
+        @login_uri = "#{signin_url}?Action=login#{signin_token_param}#{issuer_param}#{destination_param}"
+      rescue Aws::STS::Errors::AccessDenied
+        sleep 2
+        retry if (retries += 1) < 3
+      end
     end
 
   end
