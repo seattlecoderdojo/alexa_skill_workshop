7ed592966797714c43ff7988674084330f82b1b4
cwwllac <cwwllac@amazon.com>
2018-10-28 02:51:30 -0400

    only request profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_27_025337) do
+ActiveRecord::Schema.define(version: 2018_10_27_031018) do
 
   create_table "users", force: :cascade do |t|
     t.string "role"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 2018_10_27_025337) do
     t.string "provider"
     t.string "uid"
     t.string "name"
-    t.string "key_name"
   end
 
 end
